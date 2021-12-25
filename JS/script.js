e7054450ad339a00bc4ea5e3cddfff3d8b2bf157
Aleksey <34479105+AlexejMelnikov@users.noreply.github.com>
2021-12-25 12:59:57 +0300

    jsFinal

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,23 @@
-class Tasks {
+let butt = document.querySelector(".add-action"),
+  input = document.querySelector(".text-action"),
+  tasks = document.querySelector(".tasks");
+class TaskList {
   constructor(text) {
-    this.tasks = [];
+    this.tasks = ["clean"];
     this.descr = text;
   }
+  addTask(text) {
+    this.tasks.push(text);
+  }
 }
-let Task = new Tasks("ASDF");
-console.log(Task);
+let Task = new TaskList();
+Task.addTask("Clean flat");
+// console.log(Task);
+butt.addEventListener("click", (e) => {
+  e.preventDefault();
+  const paragraf = document.createElement("p");
+  paragraf.textContent = input.value;
+  tasks.append(paragraf);
+  console.log(input.value);
+  input.value = "";
+});
