3087f9e7a39f1c3095773785ffd77e1c00fcb6ca
Aleksey <34479105+AlexejMelnikov@users.noreply.github.com>
2021-12-26 23:50:33 +0300

    Добавил эффект наведения на кнопку

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
-let butt = document.querySelector(".add-action"),
+let butt = document.querySelector("button.add-action"),
   input = document.querySelector(".text-action"),
-  tasks = document.querySelector(".tasks");
+  tasks = document.querySelector(".tasks"),
+  sortDesc = document.querySelector(".sort-desc"),
+  sortAsc = document.querySelector(".sort-asc"),
+  imgButton = document.querySelector(".img-button");
+console.log(butt);
 class TaskList {
   constructor(text) {
     this.tasks = ["clean"];
@@ -9,15 +13,31 @@ class TaskList {
   addTask(text) {
     this.tasks.push(text);
   }
+  hoverButton(e) {
+    e.target.style.backGroundColor = black;
+  }
 }
 let Task = new TaskList();
 Task.addTask("Clean flat");
 // console.log(Task);
 butt.addEventListener("click", (e) => {
   e.preventDefault();
-  const paragraf = document.createElement("p");
+  // созддание параграфа для текста
+  const paragraf = document.createElement("p"),
+    img = document.createElement("img");
+  img.src = "IMG/TaskImg/Group 77.png";
+  img.classList.add("remove");
   paragraf.textContent = input.value;
+  paragraf.append(img);
   tasks.append(paragraf);
-  console.log(input.value);
+  // console.log(input.value);
   input.value = "";
+});
+butt.addEventListener("mouseenter", (e) => {
+  e.target.style.backgroundColor = "#9953F1";
+  imgButton.style.backgroundColor = "#AA68FE";
+});
+butt.addEventListener("mouseleave", (e) => {
+  e.target.style.backgroundColor = "#833AE0";
+  imgButton.style.backgroundColor = "#9953F1";
 });
